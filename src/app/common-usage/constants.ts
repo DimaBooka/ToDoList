fdd4533dc883866d3d62d13a4086e3e48a40a9ed
DimaBooka <atlasb777@gmail.com>
2017-06-23 23:28:13 +0300

    changed api_path

--- src/app/common-usage/constants.ts
+++ src/app/common-usage/constants.ts
@@ -1,2 +1,1 @@
-// export const API_PATH = 'https://todo-list-test-753.herokuapp.com/api/items';
-export const API_PATH = 'http://localhost:8080/api/items';
+export const API_PATH = 'https://todo-list-test-753.herokuapp.com/api/items';
